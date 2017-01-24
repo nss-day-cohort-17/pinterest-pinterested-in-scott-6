172be4ac7a0d7d85f3452f0c659621adf9555141
joellegg <joellegg@bellsouth.net>
2017-01-24 14:56:55 -0600

    get firebasefactory data

--- app/controllers/newPin.js
+++ app/controllers/newPin.js
@@ -1,18 +1,27 @@
 app.controller('NewPinCrtl', function($scope, firebaseFactory){
   //console.log("NewPinCrtl");
   $scope.checkboxModel = { value : true };
-  $scope.board = {};
-  $scope.pin = {};
+  // $scope.board = {};
+  // $scope.pin = {};
 
+  // get data from firebase
+  firebaseFactory.getData().then(function(val) {
+    $scope.pin = val.data.pins;
+    $scope.board = val.data.boards;
+    console.log('pin', $scope.pin);
+    console.log('board', $scope.boards);
+  });
+
+  // push data to firebase
   $scope.addPin = function() {
     // if new board is selected, create a board then make the pin run
-   if ($scope.checkboxModel.value === true) {
-    firebaseFactory.postData($scope.board);
-   } else {
+    if ($scope.checkboxModel.value === true) {
+      firebaseFactory.postData($scope.board);
+    } else {
     firebaseFactory.postData($scope.pin);
-   }
-  // reset the form
-  $scope.board = {};
-  $scope.pin = {}
+    }
+    // reset the form
+    $scope.board = {};
+    $scope.pin = {}
   }
 })
