8e502167d0d1064bb6dc0b82fc11b509a382643f
John McCutchan <john.mccutchan.wd@gmail.com>
2017-01-23 22:58:31 -0600

    cleaned up injector error

--- app/controllers/newPin.js
+++ app/controllers/newPin.js
@@ -1,10 +1,3 @@
 console.log("NewPinCrtl");
-app.controller('NewPinCrtl', function($scope, firebaseFactory, $routeParaams, $http){
-  // $scope.pin = {};
-  // add an ng-submit to the form to run a function to push to firebase
-  //$scope.addPin = function() {
-   // firebaseFactory.postData($scope.pin);
-    //$http.post('https://mf-user-notes.firebaseio.com/.json', $scope.newNote);
-    //$scope.pin = {};
-  //}
+app.controller('NewPinCrtl', function($scope, firebaseFactory){
 })
