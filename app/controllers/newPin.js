34e5a288feccdfdd2cbfcae479207c8f860f412d
joellegg <joellegg@bellsouth.net>
2017-01-24 11:17:03 -0600

    added ability to select board and add new board  to new pin

--- app/controllers/newPin.js
+++ app/controllers/newPin.js
@@ -1,3 +1,15 @@
-console.log("NewPinCrtl");
 app.controller('NewPinCrtl', function($scope, firebaseFactory){
+  //console.log("NewPinCrtl");
+  $scope.pin = {};
+  $scope.addPin = function() {
+    firebaseFactory.postData($scope.pin);
+    //$http.post('https://mf-user-notes.firebaseio.com/.json', $scope.pin);
+    $scope.pin = {}
+  }
+
+  $scope.board = {};
+  $scope.addBoard = function() {
+    firebaseFactory.postData($scope.board);
+    $scope.board = {};
+  }
 })
