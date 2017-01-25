e495b29c800faa07d26c2d4959275b8029572603
John McCutchan <john.mccutchan.wd@gmail.com>
2017-01-25 14:02:21 -0600

    linked to firebase in the config

--- app/config.js
+++ app/config.js
@@ -1,5 +1,12 @@
 app.config(function($routeProvider, $locationProvider){
 $locationProvider.hashPrefix('!');
+    firebase.initializeApp({
+        apiKey: "AIzaSyDLQO3OdUT6AeXMIMGXPFXEFNFifZBYo4E",
+        authDomain: "pinterested-in-scott-pins.firebaseapp.com",
+        databaseURL: "https://pinterested-in-scott-pins.firebaseio.com",
+        storageBucket: "pinterested-in-scott-pins.appspot.com",
+        messagingSenderId: "293020506124"
+    });
 
     $routeProvider
         .when('/', {
@@ -19,3 +26,17 @@ $locationProvider.hashPrefix('!');
         })
 
 })
+
+//
+// <script src="https://www.gstatic.com/firebasejs/3.6.7/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyDLQO3OdUT6AeXMIMGXPFXEFNFifZBYo4E",
+//     authDomain: "pinterested-in-scott-pins.firebaseapp.com",
+//     databaseURL: "https://pinterested-in-scott-pins.firebaseio.com",
+//     storageBucket: "pinterested-in-scott-pins.appspot.com",
+//     messagingSenderId: "293020506124"
+//   };
+//   firebase.initializeApp(config);
+// </script>
