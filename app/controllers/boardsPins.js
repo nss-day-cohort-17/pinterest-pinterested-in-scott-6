cf1b3233d795abdf10c597a5570a7daf357f728e
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-27 10:12:24 -0600

    last push

--- app/controllers/boardsPins.js
+++ app/controllers/boardsPins.js
@@ -1,26 +1,23 @@
 app.controller('BoardsPinsCtrl', function($scope, firebaseFactory, pinFactory, authFactory, $location, $routeParams){
   console.log("BoardsPinsCtrl");
 
-  $scope.filteredPins = {}
+  $scope.newPin = {};
 
   pinFactory.getPins().then(function(val){
     console.log(val.data);
     $scope.pins = val.data
-
     //passes board key from board clicked and stores it
     $scope.boardKey = $routeParams.key
 
-    angular.forEach($scope.pins, function(value, key) {
-      if (value.boardKey === $scope.boardKey) {
-        console.log($scope.boardKey + '=' + value.boardKey);
-
-        
-      } else {
-
-      }
-    })
-
   })
 
+  $scope.openPinModal = () =>{
+    firebaseFactory
+    .postPin($scope.newPin)
+    .then(function(){
+      $scope.newPin = {};
+    });
+
+  }
 
 })
